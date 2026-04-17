d70fd8f2f0e623851421110f561d3ed642913c9f
DarlingOlvera <front.01@cliker.mx>
2026-04-17 16:25:06 -0600

    Juego del ahorcado

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -1,3 +1,88 @@
-"""JUEGO DEL AHORCADO
-
 """
+JUEGO DEL AHORCADO
+-se elige una palabra secreta y solo se muestran la cantidad de lineas que tiene la palabra
+-El jugador elige una letra
+-Si la letra se encuentra en la palabra, mostrar en que posicion se encuentra
+-si no se encuentra se pierde una vida
+-se tiene un total de 6 vidas
+"""
+
+from random import *
+
+
+def choose_a_word(palabras):
+    shuffle(palabras)
+    selected_word = choice(palabras)
+    return selected_word
+
+
+def verify_letter(letter, selected_word):
+    word_as_list = list(selected_word)
+    l_position = []
+    # enumerate() devuelve el índice real de cada iteración
+    # .index() siempre devuelve la primera aparición, no funciona para letras repetidas
+    for index, l in enumerate(word_as_list):
+        if l.lower() == letter.lower():
+            l_position.append(index)
+    return l_position
+
+
+def verify_word(chosen_word, game_word):
+    user_word = "".join(game_word)
+    if chosen_word.lower() == user_word.lower():
+        return True
+    else:
+        return False
+
+
+def main_game():
+    palabras = [
+        "python",
+        "programacion",
+        "computadora",
+        "teclado",
+        "monitor",
+        "javascript",
+        "desarrollo",
+        "algoritmo",
+        "variable",
+        "funcion",
+        "diccionario",
+        "lista"
+    ]
+    remaining_lifes = 6
+    word = choose_a_word(palabras)
+    word_length = len(word)
+    list_word = ['__'] * word_length
+    print("""
+          BIENVENIDO AL JUEGO DEL AHORCADO
+          - Tienes 6 intentos
+          - Adivina la palabra para ganar
+          =================================\n
+          """)
+
+    while remaining_lifes > 0:
+        print(f"Intentos restantes:{remaining_lifes}\n")
+        print(f"{list_word}\n")
+        user_letter = input("Ingresa una letra A-Z: ")
+        print("\n")
+        indexes = verify_letter(user_letter, word)
+        if len(indexes) > 0:
+            for i in indexes:
+                list_word[i] = user_letter.upper()
+        else:
+            remaining_lifes -= 1
+
+        win = verify_word(word, list_word)
+
+        if win == True:
+            print(f"{list_word}\n")
+            print("¡Felicidades! Ganaste el juego")
+            break
+
+    if win == False:
+        print("\n")
+        print(f"¡Lo siento! la palabra correcta es: {word}")
+
+
+main_game()
--- practica_funciones.py
+++ practica_funciones.py
@@ -9,8 +9,10 @@ número menor.
 Si la suma de los 3 números es un valor entre 10 y 15
 (incluidos) va a devolver el número de valorintermedio.
 """
-def devolver_distintos(num1,num2,num3):
-    lista_numeros = [num1,num2,num3]
+
+
+def devolver_distintos(num1, num2, num3):
+    lista_numeros = [num1, num2, num3]
     if sum(lista_numeros) > 15:
         return max(lista_numeros)
     elif sum(lista_numeros) < 10:
@@ -19,7 +21,8 @@ def devolver_distintos(num1,num2,num3):
         for n in lista_numeros:
             if n != max(lista_numeros) and n != min(lista_numeros):
                 return n
-            
+
+
 """
 Escribe una función (puedes ponerle cualquier nombre que
 quieras) que reciba cualquier palabra como parámetro, y que
@@ -27,9 +30,11 @@ devuelva todas sus letras únicas (sin repetir) pero en orden
 alfabético.
 """
 
+
 def letras_unicas(palabra):
     lista_letras = set(palabra.lower())
     return sorted(lista_letras)
+
 
 """
 Escribe una función que requiera una cantidad indefinida de
@@ -38,12 +43,29 @@ algún momento se ha ingresado al numero cero repetido dos
 veces consecutivas
 """
 
-def ceros_consecutivos(*args):
-    #pendiente
-    cero_count = 0
-    for val in args:
-        print(f"{val}")
 
-ceros_consecutivos(1,0,2,0,0,3)
-        
-        
+def ceros_consecutivos(*args):
+    consecutivo = False
+    for i in range(len(args) - 1):  # Iterar hasta el penúltimo elemento
+        if args[i] == 0 and args[i + 1] == 0:  # Verificar si ambos son cero
+            consecutivo = True
+            break
+    return consecutivo
+
+
+"""Escribe una función llamada contar_primos() que requiera un
+solo argumento numérico.
+Esta función va a mostrar en pantalla todos los números
+primos existentes en el rango que va desde cero hasta ese
+número incluido, y va a devolver la cantidad de números
+primos que encontró.
+Aclaración, por convención el 0 y el 1 no se consideran primos."""
+
+
+def contar_primos(size):
+    lista = [n for n in range(0, size + 1) if n % 2 == 1 and n != 1]
+    print(f"Lista de números primos: {lista}")
+    print(f"Cantidad de números encontrados: {len(lista)}")
+
+
+contar_primos(20)
